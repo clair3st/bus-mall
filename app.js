1d1120546717d7ca43e42895ba8d52774511d84f
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-03-29 10:04:11 -0700

    added functions that take a random object from the array and uses the file path to render one image to the array

--- app.js
+++ app.js
@@ -12,6 +12,29 @@ function SurveyImages(name, filePath) {
 
 var bagImg = new SurveyImages('bag', 'img/bag.jpg');
 var bananaImg = new SurveyImages('banana', 'img/banana.jpg');
+var bathroomImg = new SurveyImages('bathroom', 'img/bathroom.jpg');
 
 console.log('bag: ', bagImg);
 console.log('array: ', surveyImagesArray);
+
+function renderImages(randomObject) {
+  elBody = document.getElementById('marketResearch');
+  elImg = document.createElement('img');
+  elImg.setAttribute('class', 'surveyDisplay');
+  elImg.setAttribute('src', randomObject.filePath);
+  elBody.appendChild(elImg);
+  randomObject.timesRendered += 1;
+}
+
+console.log('random object ', randomImagesObject(surveyImagesArray));
+renderImages(randomImagesObject(surveyImagesArray));
+
+//function that call a random filePath from the Array
+function randomImagesObject(globalArray) {
+  return globalArray[randomNo(0, globalArray.length)];
+}
+
+//function to generate random number between inclusive min and exclusive max
+function randomNo (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
